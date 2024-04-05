c71a6a69d8bcce33b5e0bb2dc212fdd46c855406
Riccardo Ceccaroni <50733776+riccardoc95@users.noreply.github.com>
2024-04-05 12:51:57 +0200

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,27 +1,31 @@
 import numpy as np
-import numpy_dataset
+from sklearn.datasets import fetch_openml
 import os
 
-if not os.path.exists("data/temp"):
-    os.makedirs("data/temp")
+
+def load_mnist():
+    mnist = fetch_openml('mnist_784', version=1, cache=True, parser='auto')
+    X = mnist.data
+    y = mnist.target.astype(np.uint8)
+    return X, y
+
+def load_cifar10():
+    cifar = fetch_openml('CIFAR_10', version=1, cache=True, parser='auto')
+    X = cifar.data
+    y = cifar.target.astype(np.uint8)
+    return X, y
+
+
 if not os.path.exists("data/MNIST"):
     os.makedirs("data/MNIST")
 if not os.path.exists("data/CIFAR10"):
     os.makedirs("data/CIFAR10")
 
-train_images, train_labels, valid_images, valid_labels, \
-test_images, test_labels  = numpy_datasets.images.mnist.load(path="data/temp")
 
-for i, train_image in enumerate(train_images):
+X, y = load_mnist()
+for i, train_image in enumerate(X):
     np.save(f"data/MNIST/{i + 1}.npy", train_image)
-
-train_images, train_labels, test_images, test_labels \
-= numpy_datasets.images.cifar10.load(path="data/temp")
-
-for i, train_image in enumerate(train_images):
+    
+X, y = load_cifar10()
+for i, train_image in enumerate(X):
     np.save(f"data/CIFAR10/{i + 1}.npy", train_image)
-
-
-
-
-
